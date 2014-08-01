184ff10bbe1f23b9a6ed5ebb5d3b2a112baa4363
Natasha <natalia.shadrina@emc.com>
2014-07-31 20:16:46 -0400

    test adjusted

--- web/src/test/java/trisoftdp/web/core/DbWebHelperTest.java
+++ web/src/test/java/trisoftdp/web/core/DbWebHelperTest.java
@@ -6,19 +6,17 @@ import javax.servlet.ServletException;
 
 import org.junit.Test;
 
-import trisoftdp.core.ToolKit;
-
 public class DbWebHelperTest {
 
 	@Test
 	public void test() {
 		try {
-			long resId = DbWebHelper.getResultId("md5");
+			DbWebHelper.getResultId("md5");
 		} catch (ServletException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			fail(e.getMessage());			
 		}
-		fail("Not yet implemented");
+
 	}
 
 }
